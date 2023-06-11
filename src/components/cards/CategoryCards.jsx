47e4e48b5631f2841c5e23297ea7b42420d9c6f4
NightFury742 <eshaan122@gmail.com>
2023-06-11 20:34:05 +0530

    made changes to responsiveness

--- src/components/cards/CategoryCards.jsx
+++ src/components/cards/CategoryCards.jsx
@@ -1,9 +1,9 @@
 const CategoryCards = (props) => {
   return (
-    <div className="max-sm:w-72 max-sm:max-h-72 w-[400px] h-[400px]  bg-[#f1f5f9] shadow-[0px 11px 25px rgba(0, 0, 0, 0.25)] rounded-[44px] overflow-hidden m-6 flex justify-center relative">
+    <div className="w-72 h-72 sm:w-[21rem] sm:h-[21rem] 2xl:w-[24.4rem] 2xl:h-[24.4rem] lg:w-[22.5rem] lg:h-[22.5rem] bg-[#f1f5f9] shadow-[0_7.33px_16.67px_rgba(0,0,0,0.25)] rounded-[44px] overflow-hidden m-6 flex justify-center relative">
       <img className="opacity-[0.23] absolute left-[97px] top-[110px]" src={`../../../public/${props.card.image}`} />
       <div className="flex items-center">
-        <p className="max-sm:text-3xl monu text-[#37474F] shadow-[5px 4px 4px rgba(0, 0, 0, 0.25)] text-[41px] font-normal leading-[70px] ">
+        <p className="text-3xl sm:text-[2rem]  lg:text-4xl  monu text-[#37474F] shadow-[5px 4px 4px rgba(0, 0, 0, 0.25)] font-normal  ">
           {props.card.domain}
         </p>
       </div>
--- src/pages/LandingPage/content.jsx
+++ src/pages/LandingPage/content.jsx
@@ -7,10 +7,10 @@ const Content = () => {
   });
   return (
     <div className="text-center">
-      <p className="comfort pl-4 pr-4  m-0 max-sm:text-[50px]  max-sm:tracking-[-0.4rem]  max-sm:leading-[60px]  font-bold text-[90px] leading-[100px]  text-[#0f172A] tracking-[-0.5rem]">
+      <p className="comfort pl-4 pr-4  m-0 text-[50px] tracking-[-0.4rem] leading-[60px] md:text-[4rem] 2xl:text-[5.3rem] 2xl:leading-[6.25rem] 2xl:tracking-[-0.5rem]  text-[#0f172A] ">
         Choose Your Topic !
       </p>
-      <img className="pl-4 pr-4 mx-auto mb-[4rem] max-sm:mb-8" src={underline} alt="underline" />
+      <img className="pl-4 pr-4 mx-auto mb-[4rem] " src={underline} alt="underline" />
       <div className=" flex flex-wrap justify-center ">{card}</div>
     </div>
   );
