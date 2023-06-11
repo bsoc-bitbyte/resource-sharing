e4c8444784cd5a5f21e7f35f5c88171d7a78567a
Eshaan Modi <119070798+NightFury742@users.noreply.github.com>
2023-06-11 15:32:49 +0530

    Cards component added. (#19)
    * added cards
    
    * fix: minor fixes in config for vite, tailwindcss, postcss
    
    * Create contributing.md (#14)
    
    created contributing.md
    
    Co-authored-by: Sambhav Gupta <81870866+sambhavgupta0705@users.noreply.github.com>
    
    * card component completed
    
    * made changes to responsiveness
    
    ---------
    
    Co-authored-by: Anurag607 <maskeddeep@gmail.com>
    Co-authored-by: Charan2437 <129493881+Charan2437@users.noreply.github.com>
    Co-authored-by: Sambhav Gupta <81870866+sambhavgupta0705@users.noreply.github.com>

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import NavBar from '../../components/NavBar'
-import HeroSection from '../../components/HeroSection'
-import Content from './content'
-import Footer from '../../components/Footer'
+import NavBar from '../../components/NavBar';
+import HeroSection from '../../components/HeroSection';
+import Content from './content';
+import Footer from '../../components/Footer';
 
 const LandingPage = () => {
   return (
     <>
-        <NavBar />
-        <HeroSection />
-        <Content />
-        <Footer />
+      <NavBar />
+      <HeroSection />
+      <Content />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
