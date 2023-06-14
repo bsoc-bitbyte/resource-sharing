3c9e9113e026f60025647663f6a5f0e5a3a92fdb
shraman <22bcs229@iiitdmj.ac.in>
2023-06-14 14:55:00 +0530

    Updated the Requested changes and Resolved Merge conflicts

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,6 +3,7 @@ import NavBar from '../../components/NavBar'
 import HeroSection from '../../components/HeroSection'
 import Content from './content'
 import Footer from '../../components/Footer'
+
 const LandingPage = () => {
   return (
     <>
