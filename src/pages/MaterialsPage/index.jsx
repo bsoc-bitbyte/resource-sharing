c27b53472616850b97416e6c1cc9cfe8f0a78f8e
Akash Kumar Sah <akashsah2003@gmail.com>
2023-06-17 01:32:36 +0530

    Added different fetch links and different bgimages for domains

--- src/pages/MaterialsPage/index.jsx
+++ src/pages/MaterialsPage/index.jsx
@@ -5,39 +5,57 @@ import MaterialCard from '../../components/cards/MaterialCard';
 import SearchBar from '../../components/SearchBar';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import cardData from '../../config/CardData.mjs';
 
 
 const MaterialsPage = () => {
-  const fetchmaterials = async () => {
-    try {
-      const response = await fetch('https://64870876beba6297278fb836.mockapi.io/materials');
-      const materials = await response.json();
-      setMaterials(materials);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const categoryimage = "BackgroundImage.png";
   const {category} = useParams();
-  const [materials, setMaterials] = useState([]);
-
-  useEffect(() => {
-
-    fetchmaterials();
-  }, [materials]);
-
+  let categoryid = -1;
+  cardData.map((value) => {
+    if (value.urlparams == category)
+    {
+      categoryid=value.id;
+    }
+  });
+  if (categoryid != -1)
+  {
+      const categoryimage = cardData[categoryid-1].background;
+      const [materials, setMaterials] = useState([]);
+      const fetchmaterials = async () => {
+        try {
+          const response = await fetch(cardData[categoryid-1].fetchlink);
+          const materials = await response.json();
+          setMaterials(materials);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      useEffect(() => {
+    
+        fetchmaterials();
+      }, [materials]);
+    
+      return (
+        <>
+          <NavBar />
+          <MaterialIntro image={categoryimage} length={materials.length} category={cardData[categoryid-1].domain}/>
+          <SearchBar />
+          {materials.map((material) =>
+          (
+            <MaterialCard material={material} key={material.id}/>
+          ))}
+          <Footer />
+        </>
+      );
+  };
   return (
     <>
-      <NavBar />
-      <MaterialIntro image={categoryimage} length={materials.length} category={category}/>
-      <SearchBar />
-      {materials.map((material) =>
-      (
-        <MaterialCard material={material} key={material.id}/>
-      ))}
-      <Footer />
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
     </>
-  );
+  )
 };
 
 export default MaterialsPage;
