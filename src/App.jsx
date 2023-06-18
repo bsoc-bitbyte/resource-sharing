99c66d6fd781701ad4aad71b89f0631695ca3832
owaish7 <129511171+owaish7@users.noreply.github.com>
2023-06-18 09:45:01 +0530

    Update App.jsx
    removed spaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-
 import Login from './pages/login';
 import Signup from './pages/signup';
-
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-
-          
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/signup" element={<Signup/>}></Route>
-
         </Routes>
       </BrowserRouter>
     </>
