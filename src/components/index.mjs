910dff6620d5ee1d83e83204f0d25c2d827d892b
Anurag607 <maskeddeep@gmail.com>
2023-06-19 16:15:18 +0530

    fix: minor fixes to componenet imports

--- src/components/index.mjs
+++ src/components/index.mjs
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
 import SearchBar from "./SearchBar";
-import { CategoryCards, MaterialCard, MaterialIntro } from "./cards/index.mjs";
+import { CategoryCards, MaterialCard, MaterialIntro } from "./Cards";
 
 export {
     NavBar,
