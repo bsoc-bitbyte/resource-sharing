659308b2202e1d65fb4bba14cacc3dec6ce6e0c2
Anurag607 <maskeddeep@gmail.com>
2023-06-21 14:12:37 +0530

    fix: fixed font issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { ThemeProvider } from '@material-tailwind/react';
+import "@fontsource/comfortaa";
+import "@fontsource/comfortaa/400.css";
+import "@fontsource/comfortaa/500.css";
+import "@fontsource/comfortaa/600.css";
+import "@fontsource/comfortaa/700.css";
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
