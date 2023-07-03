e3adfbd99064414a8c5dac82a025a52276526f8f
Ayush Govil <govilayush@gmail.com>
2023-07-03 17:16:51 +0530

    Added preloader for materialPages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import MaterialsPage from './pages/MaterialsPage';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import './styles/globals.css';
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/materials/:category" element={<MaterialsPage />} />
+          <Route
+            path="/materials/:category"
+            element={
+              <Suspense fallback={<div className="preloader"></div>}>
+                <MaterialsPage />
+              </Suspense>
+            }
+          />
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/signup" element={<Signup/>}></Route>
         </Routes>
