f3b27ec14042db2fbdf19769c24adaf46e9033cc
AKASH KUMAR SAH <73382164+Akashsah2003@users.noreply.github.com>
2023-07-08 15:11:02 +0530

    Implemented Login Signup and Forgot password functionality (#62)
    * Implemented Login signup reset password functionality
    
    * Error box modified highlighted error field
    
    * Error box modified highlighted error field in reset password page
    
    * custom error messages

---
+++ src/pages/forgotpassword/index.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { NavBar } from '../../components'
+import ResetPassword from './content'
+
+const Reset  = () => {
+  return (
+    <>
+      <NavBar />
+      <ResetPassword/>
+    </>
+  )
+}
+
+export default Reset
--- src/pages/signup/content.jsx
+++ src/pages/signup/content.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import { useAuthState, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '../../../Firebase/ClientApp.mjs';
+import { Navigate } from 'react-router-dom';
 
 function SignForm() {
+  const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+  const [userr, loadingg, errorrr] = useAuthState(auth);
+  const [emailid, setEmailid] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmPassword] = useState('');
+  const [errorr, setErrorr] = useState(null);
+  const [errorcause, setErrorcause] = useState('');
+  const validEmail = /^\d{2}[A-Za-z]{3}\d{3}$/;
+  const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{6,}$/;
+  const signupuser = (emailid, password, confirmpassword) => {
+    const emailexp = emailid.split('@');
+    if (!(emailexp.length==2 && emailexp[1]=='iiitdmj.ac.in' && validEmail.test(emailexp[0])))
+    {
+        setErrorr('Enter valid institute email id.');
+        setErrorcause('email');
+    }
+    else if (password!=confirmpassword)
+    {
+      setErrorr('Password and confirm pasword should match.');
+      setErrorcause('password');
+    }
+    else if (!validPassword.test(password))
+    {
+      setErrorr('Password should be atleast 6 digit long.\nPassword should contain atleast one uppercase letter, one  lowercase letter, one digit and one special symbol.')
+      setErrorcause('password');
+    }
+    else
+    {
+      createUserWithEmailAndPassword(emailid, password);
+    }
+  }
+  useEffect(()=>{
+    if (error)
+    {
+      if (error.message.toLowerCase().includes('password'))
+      {
+        setErrorcause('password');
+      }
+      else if (error.message.toLowerCase().includes('email'))
+      {
+        setErrorcause('email');
+      }
+    }
+  }, [error])
+  if (user || userr)
+  {
+    return <Navigate replace to="/" />;
+  }
   return (
     <div className=" h-screen flex items-center justify-center border-black pt-[11vh]" style={{
       backgroundImage: 'url("/loginpage-background-image.webp")',
@@ -22,24 +73,40 @@ function SignForm() {
         </div>
         <hr className=' ' />
         <form className="px-6 py-4">
+          <div className={`my-3 text-center text-base bg-red-500 text-white rounded-lg p-1 text-gray-600 ${(errorr)? 'visible' : 'hidden'}`}>
+              {(errorr) ? errorr : null}
+            </div>
+          <div className={`my-3 text-center text-base bg-red-500 text-white rounded-lg p-1 text-gray-600 capitalize ${(error)? 'visible' : 'hidden'}`}>
+              {(error) ? error.message.replaceAll('Firebase: Error (auth/', '').replaceAll(').', '').replaceAll('-', ' ') : null}
+            </div>
           <div className="mb-6">
             <label className="text-gray-500">Name</label>
             <input type="text" className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C]" required />
           </div>
           <div className="mb-6">
             <label className="text-gray-500">Institute Email ID</label>
-            <input type="email" className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C]" required />
+            <input type="email" className={`w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C] ${(errorcause=='email') ? 'border-red-500' : null}`} required onChange={(e) => setEmailid(e.target.value)}/>
           </div>
           <div className="mb-6">
             <label className="text-gray-500">Password</label>
-            <input type="password" className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C]" required />
+            <input type="password" className={`w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C] ${(errorcause=='password') ? 'border-red-500' : null}`} required onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className="mb-6">
             <label className="text-gray-500">Confirm Password</label>
-            <input type="password" className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C]" required />
+            <input type="password" className={`w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none dark:text-[#1A1A1C] ${(errorcause=='password') ? 'border-red-500' : null}`} required onChange={(e) => setConfirmPassword(e.target.value)}/>
           </div>
 
-          <input type="submit" value="SignUp" className="w-full bg-blue-500 text-white font-bold py-2 rounded-lg cursor-pointer hover:bg-blue-600" />
+          <input type="submit" value="SignUp" className="w-full bg-blue-500 text-white font-bold py-2 rounded-lg cursor-pointer hover:bg-blue-600" onClick={(e) => 
+            {
+              e.preventDefault();
+              setErrorr(null);
+              setErrorcause('');
+              signupuser(emailid, password, confirmpassword)
+            }
+            }/>
+            <div className={`flex items-center justify-center h-10 ${(loading || loading) ? 'visible' : 'hidden'}`}>
+            <img src="/ecllipseloading.svg" alt="" className='bg-white h-full' />
+          </div>
           <div className="mt-6 text-center text-base text-gray-600">
             Already a member? <a href="#" className="text-blue-500 hover:underline"><Link to="/login">Login</Link></a>
           </div>
