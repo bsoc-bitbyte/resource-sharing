f09c6e5f92f851bd214838f18722a946b923c734
jainvedant392 <jainvedant392@gmail.com>
2023-07-08 16:42:04 +0530

    feat: Modify PDFViewer index.jsx

--- src/components/PDFViewer/index.jsx
+++ src/components/PDFViewer/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { Viewer, Worker, ViewMode } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import NavBar from "../NavBar";
-import pdfFile from "../../assets/fonts/2.2_Scales.pdf";
+import pdfFile from "../../../public/2.2_Scales.pdf";
 import classNames from "classnames";
 
 export default function PDFViewer(){
@@ -18,7 +18,7 @@ export default function PDFViewer(){
                     'flex justify-center w-full': true,
                 })}>
                     <div className={classNames({
-                        'h-135 w-11/12 mt-20': true,
+                        'h-[540px] w-[1350px] mt-20': true,
                     })}>
                         <Viewer
                             fileUrl={pdfFile}
@@ -26,7 +26,7 @@ export default function PDFViewer(){
                             plugins={[
                                 defaultLayoutPluginInstance,
                             ]}
-                            className="custom-pdf-viewer"
+                            viewMode={ViewMode.DualPage}
                         />
                     </div>
                 </div>
