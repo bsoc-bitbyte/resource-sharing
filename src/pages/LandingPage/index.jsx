4fc795add6b4956c2e2609b43fb4382b48386b84
Kushagra <22bcs148@iiitdmj.ac.in>
2023-07-13 01:18:31 +0530

    added scroll to top button

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,14 +1,23 @@
 import { NavBar, HeroSection, Footer } from '../../components';
+import BackToTop from '../../components/backtotop';
 import Content from '../../components/content';
 
 const LandingPage = () => {
   return (
-    <div className={'flex flex-col items-start justify-center gap-20'}>
+    <>
+
+    <div className={'flex flex-col items-start justify-center gap-20 '}>
       <NavBar />
       <HeroSection />
       <Content />
       <Footer />
     </div>
+
+    <div>
+      <BackToTop />
+    </div>
+
+    </>
   );
 };
 
