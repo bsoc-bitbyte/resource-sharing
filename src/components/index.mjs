c1f792affa717191a486b539c14aca247e947b2a
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-07-12 17:35:07 +0530

    frontend fixes

--- src/components/index.mjs
+++ src/components/index.mjs
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
 import SearchBar from "./SearchBar";
-import { CategoryCards, MaterialCard, MaterialIntro } from "./Cards";
+import { CategoryCards, MaterialCard, MaterialIntro } from "./cards";
 
 export {
     NavBar,
