cd8e3d7b7709e7a5c1691c53f676c7f13eafd579
Kushdapush <129064700+Kushdapush@users.noreply.github.com>
2023-07-17 18:02:45 +0530

    Made the dashboard component (#81)
    * Made the dashboard component
    
    * made the requested changes
    
    * made the requested changes
    
    * made some more UI changes as instructed
    
    * made changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import Layout from './pages/OverviewPage';
 import PageNoteFound from './pages/404page'
-
 import PDFViewer from './components/PDFViewer';
 import Reset from './pages/forgotpassword'
+import Dashboard from './components/Dashboard';
+import Uploads from './components/Dashboard/uploads';
+import Downloads from './components/Dashboard/downloads';
+import Bookmarks from './components/Dashboard/bookmarks';
 const App = () => {
   return (
     <>
@@ -21,7 +24,12 @@ const App = () => {
           <Route path="/details" element={<Layout/>}></Route>
           <Route path="/pdfviewer/:id" element={<PDFViewer/>}></Route>
           <Route path="/resetpassword" element={<Reset/>}></Route>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
           <Route path="*" element={<PageNoteFound/>}></Route>
+          <Route path="/uploads" element={<Uploads />}></Route>
+          <Route path="/downloads" element={<Downloads />}></Route>
+          <Route path="/bookmarks" element={<Bookmarks />}></Route>
+
         </Routes>
       </BrowserRouter>
     </>
--- src/components/index.mjs
+++ src/components/index.mjs
@@ -2,6 +2,7 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
 import SearchBar from "./SearchBar";
+import Dashboard from "./Dashboard";
 import BackToTop from "./backtotop";
 import { CategoryCards, MaterialCard, MaterialIntro } from "./cards";
 
@@ -11,6 +12,7 @@ export {
     Footer,
     HeroSection,
     SearchBar,
+    Dashboard,
     CategoryCards,
     MaterialCard,
     MaterialIntro,
